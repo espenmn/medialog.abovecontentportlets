ade413833e349abaac3ef3dfe749550778de042d
Espen Moe-Nilssen <espen@medialog.no>
2022-02-02 15:01:44 +0100

    Fix for 5.2 (remove plone.directives)

--- src/medialog/abovecontentportlets/interfaces.py
+++ src/medialog/abovecontentportlets/interfaces.py
@@ -3,11 +3,11 @@
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from plone.app.portlets.interfaces import IColumn
-    
+
 from z3c.form import interfaces
-from zope import schema
-from zope.interface import alsoProvides
-from plone.directives import form
+#from zope import schema
+#from zope.interface import alsoProvides
+#from plone.directives import form
 
 
 class IMedialogAboveContentPortletsLayer(IDefaultBrowserLayer):
@@ -15,9 +15,8 @@ class IMedialogAboveContentPortletsLayer(IDefaultBrowserLayer):
 
 class IBelow(IColumn):
     """here we put the below content portlets
-    """    
+    """
 
 class IAbove(IColumn):
     """here we put the above content portlets
-    """    
-
+    """
